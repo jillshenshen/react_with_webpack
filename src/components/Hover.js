7fc0a78bfbb9fa71aa8478a02a87b1dbb9b15bc9
Jill shen <pinkastro@gmail.com>
2023-02-16 10:44:21 +0800

    hover effect

---
+++ src/components/Hover.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Hover() {
+  return (
+    <div class="square one">
+    <div class="cover">
+        <img src="https://image.tmdb.org/t/p/w500/xDMIl84Qo5Tsu62c9DGWhmPI67A.jpg" alt="" />
+    </div>
+    <div class="text">
+    <iframe src={`https://www.youtube.com/embed/j_MuZmJNirw?accelerometer=1&autoplay=1&mute=1`} allow="accelerometer;autoplay;clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+      Never Gonna Give You Up!
+    </div>
+  </div>
+  )
+}
+
+
+             
+
